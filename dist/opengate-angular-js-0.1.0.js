7b8401db1d3589a0083bbde80991c9603e8abfbe
José María Pérez <josemaria.perez@amplia.es>
2017-07-04 14:32:53 +0200

    ogapi error parser

--- dist/opengate-angular-js-0.1.0.js
+++ dist/opengate-angular-js-0.1.0.js
@@ -24,6 +24,41 @@ angular.module('opengate-angular-js')
         return new OgApiService();
     });
 
+angular.module('opengate-angular-js').service('$ogapiErrorParser', [
+    function() {
+        return {
+            toString: function(error) {
+                var errorMessage = '';
+                if (error.data && (error.data.errors && error.data.errors.length > 0)) {
+                    for (var i = 0; i < error.data.errors.length; i++) {
+                        errorMessage += error.data.errors[i].message + '\n';
+                    }
+                } else if (!error.data) {
+                    errorMessage = error;
+                } else {
+                    errorMessage = error.data;
+                }
+
+                return errorMessage;
+            },
+            toStringArray: function(error) {
+                var errorMessage = [];
+                if (error.data && (error.data.errors && error.data.errors.length > 0)) {
+                    for (var i = 0; i < error.data.errors.length; i++) {
+                        errorMessage.push(error.data.errors[i].message);
+                    }
+                } else if (!error.data) {
+                    errorMessage.push(error);
+                } else {
+                    errorMessage.push(error.data);
+                }
+
+                return errorMessage;
+            }
+        };
+    }
+]);
+
 
 // Filter service
 angular.module('opengate-angular-js')

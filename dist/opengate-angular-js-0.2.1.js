5a1228e84498af9ae1b1a84a45dd0129518d73bd
Mónica C. Fernández <monica.fernandez@amplia.es>
2017-07-17 15:30:25 +0200

    OUW-702: nuevos metodos para el servicio oguxThemes

--- dist/opengate-angular-js-0.2.1.js
+++ dist/opengate-angular-js-0.2.1.js
@@ -5,7 +5,11 @@ angular.module('opengate-angular-js', []);
 angular.module("opengate-angular-js").run(["$templateCache", function($templateCache) {$templateCache.put("views/window-time.select.view.html","<div class=window-time-container><button type=button class=\"btn btn-xs\" ng-class=oneDayClass ng-click=oneDay()>Last day</button> <button type=button class=\"btn btn-xs\" ng-class=oneWeekClass ng-click=oneWeek()>Last 7 days</button> <button type=button class=\"btn btn-xs\" ng-class=oneMonthClass ng-click=oneMonth()>Last 30 days</button> <button type=button class=\"btn btn-xs\" ng-class=customClass ng-click=custom()>Custom</button> <button type=button class=\"btn btn-xs btn-info\" ng-if=customEnabled ng-disabled=!!errorCustomWindow ng-click=applyCustom()>Apply</button> <button type=button class=\"btn btn-xs btn-info\" ng-if=filterApplied ng-click=clear()>Clear</button><div ng-if=customEnabled class=window-time-body><div class=row><div class=col-xs-12><p class=input-group><label class=control-label>From: {{fromDate | date:\'fullDate\'}}</label> <input readonly datepicker-options=fromOptions type=text class=form-control show-button-bar=false uib-datepicker-popup={{format}} ng-model=date.from is-open=fromPopup.opened ng-required=true close-text=Close ng-change=fromChange()> <span class=input-group-btn><a class=\"btn btn-sm\" ng-click=fromOpen()><i class=\"glyphicon glyphicon-calendar\"></i></a></span></p></div><div class=col-xs-12><div uib-timepicker max=fromMax ng-model=date.from show-meridian=false ng-change=fromChange()></div></div></div><div class=row><div class=col-xs-12><p class=input-group><label class=control-label>To: {{toDate | date:\'fullDate\'}}</label> <input readonly datepicker-options=toOptions type=text class=form-control show-button-bar=false uib-datepicker-popup={{format}} ng-model=date.to is-open=toPopup.opened ng-required=true close-text=Close ng-change=toChange()> <span class=input-group-btn><button type=button class=\"btn btn-sm\" ng-click=toOpen()><i class=\"glyphicon glyphicon-calendar\"></i></button></span></p></div><div class=col-xs-12><div uib-timepicker max=toMax min=toMin ng-model=date.to show-meridian=false ng-change=toChange()></div></div></div><alert type=danger ng-show=errorCustomWindow class=text-danger style=\"display: block;text-align: center;\"><span ng-bind=errorCustomWindow></span></alert></div></div>");}]);
 
 angular.module('opengate-angular-js').service('$oguxThemes', [
-    function() {
+    function () {
+        var themeCompositionTheme = 'light';
+        var themeCompositionColor = 'orange';
+        var themeComposition = themeCompositionTheme + ' ' + themeCompositionColor;
+
         var themes = [{
             'name': 'default'
         }, {
@@ -16,150 +20,182 @@ angular.module('opengate-angular-js').service('$oguxThemes', [
             'red': {
                 'id': 'red',
                 'sample': '#f44336',
+                'rgba': 'rgba(244, 67, 54, 0.5)',
                 'name': 'Red',
                 'theme': ['light', 'default']
             },
             'pink': {
                 'id': 'pink',
                 'sample': '#e91e63',
+                'rgba': 'rgba(233, 30, 99, 0.5)',
                 'name': 'Pink',
                 'theme': ['light', 'default']
             },
             'purple': {
                 'id': 'purple',
                 'sample': '#9c27b0',
+                'rgba': 'rgba(156, 39, 176, 0.5)',
                 'name': 'Purple',
                 'theme': ['light']
             },
             'deeppurple': {
                 'id': 'deeppurple',
                 'sample': '#673ab7',
+                'rgba': 'rgba(103, 10, 183, 0.5)',
                 'name': 'Deep Purple',
                 'theme': ['light']
             },
             'indigo': {
                 'id': 'indigo',
                 'sample': '#3f51b5',
+                'rgba': 'rgba(63, 81, 181, 0.5)',
                 'name': 'Indigo',
                 'theme': ['light']
             },
             'blue': {
                 'id': 'blue',
                 'sample': '#2196f3',
+                'rgba': 'rgba(33, 150, 243, 0.5)',
                 'name': 'Blue',
                 'theme': ['light', 'default']
             },
             'lightblue': {
                 'id': 'lightblue',
                 'sample': '#03a9f4',
+                'rgba': 'rgba(3, 169, 244, 0.5)',
                 'name': 'Light Blue',
                 'theme': ['light', 'default']
             },
             'cyan': {
                 'id': 'cyan',
                 'sample': '#00bcd4',
+                'rgba': 'rgba(0, 188, 212, 0.5)',
                 'name': 'Cyan',
                 'theme': ['light', 'default']
             },
             'teal': {
                 'id': 'teal',
                 'sample': '#009688',
+                'rgba': 'rgba(0, 150, 136, 0.5)',
                 'name': 'Teal',
                 'theme': ['light', 'default']
             },
             'green': {
                 'id': 'green',
                 'sample': '#4caf50',
+                'rgba': 'rgba(76, 175, 80, 0.5)',
                 'name': 'Green',
                 'theme': ['light', 'default']
             },
             'lightgreen': {
                 'id': 'lightgreen',
                 'sample': '#8bc34a',
+                'rgba': 'rgba(139, 195, 74, 0.5)',
                 'name': 'Light Green',
                 'theme': ['light', 'default']
             },
             'lime': {
                 'id': 'lime',
                 'sample': '#cddc39',
+                'rgba': 'rgba(205, 220, 57, 0.5)',
                 'name': 'Lime',
                 'theme': ['default']
             },
             'yellow': {
                 'id': 'yellow',
                 'sample': '#ffeb3b',
+                'rgba': 'rgba(255, 235, 59, 0.5)',
                 'name': 'Yellow',
                 'theme': ['default']
             },
             'amber': {
                 'id': 'amber',
                 'sample': '#ffc107',
+                'rgba': 'rgba(255, 193, 7, 0.5)',
                 'name': 'Amber',
                 'theme': ['default']
             },
             'orange': {
                 'id': 'orange',
                 'sample': '#ff9800',
+                'rgba': 'rgba(255, 152, 0, 0.5)',
                 'name': 'Orange',
                 'theme': ['light', 'default']
             },
             'deeporange': {
                 'id': 'deeporange',
                 'sample': '#ff5722',
+                'rgba': 'rgba(255, 87, 34, 0.5)',
                 'name': 'Deep Orange',
                 'theme': ['light', 'default']
             },
             'brown': {
                 'id': 'brown',
                 'sample': '#795548',
+                'rgba': 'rgba(121, 85, 72, 0.5)',
                 'name': 'Brown',
                 'theme': ['light']
             },
             'grey': {
                 'id': 'grey',
                 'sample': '#9e9e9e',
+                'rgba': 'rgba(33, 150, 243, 1)',
                 'name': 'Grey',
                 'theme': ['light', 'default']
             },
             'bluegrey': {
                 'id': 'bluegrey',
                 'sample': '#607d8b',
+                'rgba': 'rgba(96, 125, 139, 0.5)',
                 'name': 'Blue Grey',
                 'theme': ['light', 'default']
             },
-            'darkgrey': {
+            'rgbagrey': {
                 'id': 'darkgrey',
                 'sample': '#2D2D2D',
+                'rgba': 'rgba(45, 45, 45, 0.5)',
                 'name': 'Dark Grey',
                 'theme': ['light']
             }
         };
 
         return {
-            colors: function() {
+            setThemeComposition: function (_themeComposition) {
+                themeComposition = _themeComposition;
+                var tc = themeComposition.split(' ');
+                themeCompositionTheme = tc[0];
+                themeCompositionColor = tc[1];
+            },
+            colors: function () {
                 return colorThemes;
             },
-            themes: function() {
+            themes: function () {
                 return themes;
             },
-            getColorConfig: function(colorId) {
+            getColorConfig: function (colorId) {
                 if (colorThemes[colorId]) {
                     return colorThemes[colorId];
                 } else {
                     return undefined;
                 }
             },
-            getColorsKeys: function() {
+            getColorsKeys: function () {
                 return Object.keys(colorThemes);
             },
-            colorsByTheme: function(theme) {
+            colorsByTheme: function (theme) {
                 var themeColors = [];
-                angular.forEach(colorThemes, function(config, color) {
+                angular.forEach(colorThemes, function (config, color) {
                     if (config.theme.indexOf(theme) !== -1) {
                         themeColors.push(config);
                     }
                 });
                 return themeColors;
+            },
+            getThemeFromThemeComposition: function () {
+                return themeCompositionTheme;
+            },
+            getColorFromThemeComposition: function () {
+                return colorThemes[themeCompositionColor];
             }
         };
     }
--- src/services/oguxThemes.client.service.js
+++ src/services/oguxThemes.client.service.js
@@ -1,6 +1,10 @@
 'use strict';
 angular.module('opengate-angular-js').service('$oguxThemes', [
-    function() {
+    function () {
+        var themeCompositionTheme = 'light';
+        var themeCompositionColor = 'orange';
+        var themeComposition = themeCompositionTheme + ' ' + themeCompositionColor;
+
         var themes = [{
             'name': 'default'
         }, {
@@ -11,150 +15,182 @@ angular.module('opengate-angular-js').service('$oguxThemes', [
             'red': {
                 'id': 'red',
                 'sample': '#f44336',
+                'rgba': 'rgba(244, 67, 54, 0.5)',
                 'name': 'Red',
                 'theme': ['light', 'default']
             },
             'pink': {
                 'id': 'pink',
                 'sample': '#e91e63',
+                'rgba': 'rgba(233, 30, 99, 0.5)',
                 'name': 'Pink',
                 'theme': ['light', 'default']
             },
             'purple': {
                 'id': 'purple',
                 'sample': '#9c27b0',
+                'rgba': 'rgba(156, 39, 176, 0.5)',
                 'name': 'Purple',
                 'theme': ['light']
             },
             'deeppurple': {
                 'id': 'deeppurple',
                 'sample': '#673ab7',
+                'rgba': 'rgba(103, 10, 183, 0.5)',
                 'name': 'Deep Purple',
                 'theme': ['light']
             },
             'indigo': {
                 'id': 'indigo',
                 'sample': '#3f51b5',
+                'rgba': 'rgba(63, 81, 181, 0.5)',
                 'name': 'Indigo',
                 'theme': ['light']
             },
             'blue': {
                 'id': 'blue',
                 'sample': '#2196f3',
+                'rgba': 'rgba(33, 150, 243, 0.5)',
                 'name': 'Blue',
                 'theme': ['light', 'default']
             },
             'lightblue': {
                 'id': 'lightblue',
                 'sample': '#03a9f4',
+                'rgba': 'rgba(3, 169, 244, 0.5)',
                 'name': 'Light Blue',
                 'theme': ['light', 'default']
             },
             'cyan': {
                 'id': 'cyan',
                 'sample': '#00bcd4',
+                'rgba': 'rgba(0, 188, 212, 0.5)',
                 'name': 'Cyan',
                 'theme': ['light', 'default']
             },
             'teal': {
                 'id': 'teal',
                 'sample': '#009688',
+                'rgba': 'rgba(0, 150, 136, 0.5)',
                 'name': 'Teal',
                 'theme': ['light', 'default']
             },
             'green': {
                 'id': 'green',
                 'sample': '#4caf50',
+                'rgba': 'rgba(76, 175, 80, 0.5)',
                 'name': 'Green',
                 'theme': ['light', 'default']
             },
             'lightgreen': {
                 'id': 'lightgreen',
                 'sample': '#8bc34a',
+                'rgba': 'rgba(139, 195, 74, 0.5)',
                 'name': 'Light Green',
                 'theme': ['light', 'default']
             },
             'lime': {
                 'id': 'lime',
                 'sample': '#cddc39',
+                'rgba': 'rgba(205, 220, 57, 0.5)',
                 'name': 'Lime',
                 'theme': ['default']
             },
             'yellow': {
                 'id': 'yellow',
                 'sample': '#ffeb3b',
+                'rgba': 'rgba(255, 235, 59, 0.5)',
                 'name': 'Yellow',
                 'theme': ['default']
             },
             'amber': {
                 'id': 'amber',
                 'sample': '#ffc107',
+                'rgba': 'rgba(255, 193, 7, 0.5)',
                 'name': 'Amber',
                 'theme': ['default']
             },
             'orange': {
                 'id': 'orange',
                 'sample': '#ff9800',
+                'rgba': 'rgba(255, 152, 0, 0.5)',
                 'name': 'Orange',
                 'theme': ['light', 'default']
             },
             'deeporange': {
                 'id': 'deeporange',
                 'sample': '#ff5722',
+                'rgba': 'rgba(255, 87, 34, 0.5)',
                 'name': 'Deep Orange',
                 'theme': ['light', 'default']
             },
             'brown': {
                 'id': 'brown',
                 'sample': '#795548',
+                'rgba': 'rgba(121, 85, 72, 0.5)',
                 'name': 'Brown',
                 'theme': ['light']
             },
             'grey': {
                 'id': 'grey',
                 'sample': '#9e9e9e',
+                'rgba': 'rgba(33, 150, 243, 1)',
                 'name': 'Grey',
                 'theme': ['light', 'default']
             },
             'bluegrey': {
                 'id': 'bluegrey',
                 'sample': '#607d8b',
+                'rgba': 'rgba(96, 125, 139, 0.5)',
                 'name': 'Blue Grey',
                 'theme': ['light', 'default']
             },
-            'darkgrey': {
+            'rgbagrey': {
                 'id': 'darkgrey',
                 'sample': '#2D2D2D',
+                'rgba': 'rgba(45, 45, 45, 0.5)',
                 'name': 'Dark Grey',
                 'theme': ['light']
             }
         };
 
         return {
-            colors: function() {
+            setThemeComposition: function (_themeComposition) {
+                themeComposition = _themeComposition;
+                var tc = themeComposition.split(' ');
+                themeCompositionTheme = tc[0];
+                themeCompositionColor = tc[1];
+            },
+            colors: function () {
                 return colorThemes;
             },
-            themes: function() {
+            themes: function () {
                 return themes;
             },
-            getColorConfig: function(colorId) {
+            getColorConfig: function (colorId) {
                 if (colorThemes[colorId]) {
                     return colorThemes[colorId];
                 } else {
                     return undefined;
                 }
             },
-            getColorsKeys: function() {
+            getColorsKeys: function () {
                 return Object.keys(colorThemes);
             },
-            colorsByTheme: function(theme) {
+            colorsByTheme: function (theme) {
                 var themeColors = [];
-                angular.forEach(colorThemes, function(config, color) {
+                angular.forEach(colorThemes, function (config, color) {
                     if (config.theme.indexOf(theme) !== -1) {
                         themeColors.push(config);
                     }
                 });
                 return themeColors;
+            },
+            getThemeFromThemeComposition: function () {
+                return themeCompositionTheme;
+            },
+            getColorFromThemeComposition: function () {
+                return colorThemes[themeCompositionColor];
             }
         };
     }

1cab4905d3c899aef150291225839281b3f9d20d
José María Pérez <josemaria.perez@amplia.es>
2017-09-13 16:25:48 +0200

    Parseo de errores de WS actualizado m3

--- dist/opengate-angular-js-0.4.3.js
+++ dist/opengate-angular-js-0.4.3.js
@@ -226,18 +226,30 @@ angular.module('opengate-angular-js')
     });
 
 angular.module('opengate-angular-js').service('$ogapiErrorParser', [
+    // EXAMPLE
+    // [{
+    //     "code": 20481,
+    //     "description": "Filter path does not exists",
+    //     "context": [{
+    //         "name": "Filter field path",
+    //         "value": "datamodel.organizationName"
+    //     }]
+    // }]
+
     function() {
         return {
-            toString: function(error) {
+            toString: function(error, errorSeparatorString) {
                 var errorMessage = '';
                 if (error.data && (error.data.errors && error.data.errors.length > 0)) {
                     for (var i = 0; i < error.data.errors.length; i++) {
-                        errorMessage += error.data.errors[i].message + '\n';
+                        errorMessage += error.data.errors[i].message + (errorSeparatorString ? errorSeparatorString : '\n');
                     }
-                } else if (!error.data) {
-                    errorMessage = error;
+                } else if (error.data && error.data.length > 0) {
+                    for (var i = 0; i < error.data.length; i++) {
+                        errorMessage += error.data[i].description + (errorSeparatorString ? errorSeparatorString : '\n');
+                    }
                 } else {
-                    errorMessage = error.data;
+                    errorMessage = error;
                 }
 
                 return errorMessage;
@@ -248,8 +260,10 @@ angular.module('opengate-angular-js').service('$ogapiErrorParser', [
                     for (var i = 0; i < error.data.errors.length; i++) {
                         errorMessage.push(error.data.errors[i].message);
                     }
-                } else if (!error.data) {
-                    errorMessage.push(error);
+                } else if (error.data && error.data.length > 0) {
+                    for (var i = 0; i < error.data.length; i++) {
+                        errorMessage.push(error.data[i].description);
+                    }
                 } else {
                     errorMessage.push(error.data);
                 }

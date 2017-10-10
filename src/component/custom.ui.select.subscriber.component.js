8a6d6f2eff0a88700c0a8a175cf70519ccdbbe5b
José María Pérez <josemaria.perez@amplia.es>
2017-10-10 13:20:39 +0200

    Componente filtrombo subscriber

---
+++ src/component/custom.ui.select.subscriber.component.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+angular.module('opengate-angular-js').controller('customUiSelectSubscriberController', ['$scope', '$element', '$attrs', '$api', function($scope, $element, $attrs, $api) {
+    var ctrl = this;
+    ctrl.ownConfig = {
+        builder: $api().subscribersSearchBuilder(),
+        filter: function(search) {
+            return {
+                'or': [
+                    { 'like': { 'provision.device.communicationModules[].subscriber.identifier': search } },
+                    { 'like': { 'device.communicationModules[].subscriber.identifier': search } }
+                ]
+            };
+        },
+        rootKey: 'devices',
+        collection: [],
+        customSelectors: $api().subscribersSearchBuilder()
+    };
+
+    ctrl.entitySelected = function($item, $model) {
+        var return_obj = {};
+        return_obj['$item'] = $item;
+        return_obj['$model'] = $model;
+        ctrl.onSelectItem(return_obj);
+    };
+
+    ctrl.entityRemove = function($item, $model) {
+        ctrl.onRemove($item, $model);
+    };
+}]);
+
+angular.module('opengate-angular-js').component('customUiSelectSubscriber', {
+
+    templateUrl: 'views/custom.ui.select.entity.html',
+    controller: 'customUiSelectSubscriberController',
+    bindings: {
+        onSelectItem: '&',
+        onRemove: '&',
+        entity: '=',
+        multiple: '@',
+        isRequired: '@'
+    }
+
+});

92f79870269d3ee55dd2517d1ed56f3e3b89bb74
kmlamplia <katherin.moscoso@amplia.es>
2018-01-16 17:47:07 +0100

    Refactorizacion del metodo ogapiErrorParser

--- dist/opengate-angular-js-3.0.0.js
+++ dist/opengate-angular-js-3.0.0.js
@@ -283,57 +283,31 @@ angular.module('opengate-angular-js')
         return new OgApiService();
     });
 
-angular.module('opengate-angular-js').service('$ogapiErrorParser', [
+angular.module('opengate-angular-js').service('$ogapiErrorParser', ['jsonPath',
     // EXAMPLE
     // [{
     //     "code": 20481,
-    //     "description": "Filter path does not exists",
+    //     "message": "Filter path does not exists",
     //     "context": [{
     //         "name": "Filter field path",
     //         "value": "datamodel.organizationName"
     //     }]
     // }]
 
-    function() {
+    function(jsonPath) {
         return {
             toString: function(error, errorSeparatorString) {
                 var errorMessage = '';
-                if (error.data && (error.data.errors && error.data.errors.length > 0)) {
-                    for (var i = 0; i < error.data.errors.length; i++) {
-                        errorMessage += (error.data.errors[i].message || error.data.errors[i].description) + (errorSeparatorString ? errorSeparatorString : '\n');
-                    }
-                } else if (error.data && error.data.length > 0) {
-                    if (angular.isArray(error.data)) {
-                        for (var i = 0; i < error.data.length; i++) {
-                            errorMessage += (error.data[i].message || error.data[i].description) + (errorSeparatorString ? errorSeparatorString : '\n');
-                        }
-                    } else {
-                        errorMessage = error.data;
-                    }
-                } else {
-                    errorMessage = error;
+                var errors = jsonPath(error, '$..message');
+
+                for (var i = 0; i < errors.length; i++) {
+                    errorMessage += errors[i] + (errorSeparatorString ? errorSeparatorString : '\n');
                 }
 
                 return errorMessage;
             },
             toStringArray: function(error) {
-                var errorMessage = [];
-                if (error.data && (error.data.errors && error.data.errors.length > 0)) {
-                    for (var i = 0; i < error.data.errors.length; i++) {
-                        errorMessage.push(error.data.errors[i].message || error.data.errors[i].description);
-                    }
-                } else if (error.data && error.data.length > 0) {
-                    if (angular.isArray(error.data)) {
-                        for (var i = 0; i < error.data.length; i++) {
-                            errorMessage.push(error.data[i].message || error.data[i].description);
-                        }
-                    } else {
-                        errorMessage.push(error.data[i]);
-                    }
-                } else {
-                    errorMessage.push(error.data);
-                }
-
+                var errorMessage = jsonPath(error, '$..message');
                 return errorMessage;
             }
         };

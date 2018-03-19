fb70a1fe0adcf7d9d7c496feba46f94d34447148
chema <chema@isengard.amplia.int>
2018-03-19 17:32:50 +0100

    themes updated

--- dist/opengate-angular-js-4.2.0.js
+++ dist/opengate-angular-js-4.2.0.js
@@ -10910,150 +10910,168 @@ angular.module('opengate-angular-js').service('$oguxThemes', [
                 'sample': '#f44336',
                 'rgba': 'rgba(244, 67, 54, 0.5)',
                 'name': 'COLOR.RED',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_red.css']
             },
             'pink': {
                 'id': 'pink',
                 'sample': '#e91e63',
                 'rgba': 'rgba(233, 30, 99, 0.5)',
                 'name': 'COLOR.PINK',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_pink.css']
             },
             'purple': {
                 'id': 'purple',
                 'sample': '#9c27b0',
                 'rgba': 'rgba(156, 39, 176, 0.5)',
                 'name': 'COLOR.PURPLE',
-                'theme': ['light']
+                'theme': ['light', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_purple.css']
             },
             'deeppurple': {
                 'id': 'deeppurple',
                 'sample': '#673ab7',
                 'rgba': 'rgba(103, 10, 183, 0.5)',
                 'name': 'COLOR.DEEP_PURPLE',
-                'theme': ['light']
+                'theme': ['light', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_deeppurple.css']
             },
             'indigo': {
                 'id': 'indigo',
                 'sample': '#3f51b5',
                 'rgba': 'rgba(63, 81, 181, 0.5)',
                 'name': 'COLOR.INDIGO',
-                'theme': ['light']
+                'theme': ['light', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_indigo.css']
             },
             'blue': {
                 'id': 'blue',
                 'sample': '#2196f3',
                 'rgba': 'rgba(33, 150, 243, 0.5)',
                 'name': 'COLOR.BLUE',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_blue.min.css']
+            },
+            'freshblue': {
+                'id': 'freshblue',
+                'sample': '#2196f3',
+                'rgba': 'rgba(33, 150, 243, 0.5)',
+                'name': 'COLOR.BLUE',
+                'theme': ['fresh'],
+                'url': ['css/themes/bootstrap-material-design_freshblue.min.css']
             },
             'lightblue': {
                 'id': 'lightblue',
                 'sample': '#03a9f4',
                 'rgba': 'rgba(3, 169, 244, 0.5)',
                 'name': 'COLOR.LIGTH_BLUE',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_lightblue.min.css']
             },
             'cyan': {
                 'id': 'cyan',
                 'sample': '#00bcd4',
                 'rgba': 'rgba(0, 188, 212, 0.5)',
                 'name': 'COLOR.CYAN',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_cyan.min.css']
             },
             'teal': {
                 'id': 'teal',
                 'sample': '#009688',
                 'rgba': 'rgba(0, 150, 136, 0.5)',
                 'name': 'COLOR.TEAL',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_teal.min.css']
             },
             'green': {
                 'id': 'green',
                 'sample': '#4caf50',
                 'rgba': 'rgba(76, 175, 80, 0.5)',
                 'name': 'COLOR.GREEN',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_green.min.css']
             },
             'lightgreen': {
                 'id': 'lightgreen',
                 'sample': '#8bc34a',
                 'rgba': 'rgba(139, 195, 74, 0.5)',
                 'name': 'COLOR.LIGHT_GREEN',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_lightgreen.min.css']
             },
             'lime': {
                 'id': 'lime',
                 'sample': '#cddc39',
                 'rgba': 'rgba(205, 220, 57, 0.5)',
                 'name': 'COLOR.LIME',
-                'theme': ['default']
+                'theme': ['default'],
+                'url': ['css/themes/bootstrap-material-design_lime.min.css']
             },
             'yellow': {
                 'id': 'yellow',
                 'sample': '#ffeb3b',
                 'rgba': 'rgba(255, 235, 59, 0.5)',
                 'name': 'COLOR.YELLOW',
-                'theme': ['default']
+                'theme': ['default'],
+                'url': ['css/themes/bootstrap-material-design_yellow.min.css']
             },
             'amber': {
                 'id': 'amber',
                 'sample': '#ffc107',
                 'rgba': 'rgba(255, 193, 7, 0.5)',
                 'name': 'COLOR.AMBER',
-                'theme': ['default']
+                'theme': ['default'],
+                'url': ['css/themes/bootstrap-material-design_amber.min.css']
             },
             'orange': {
                 'id': 'orange',
                 'sample': '#ff9800',
                 'rgba': 'rgba(255, 152, 0, 0.5)',
                 'name': 'COLOR.ORANGE',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed']
             },
             'deeporange': {
                 'id': 'deeporange',
                 'sample': '#ff5722',
                 'rgba': 'rgba(255, 87, 34, 0.5)',
                 'name': 'COLOR.DEEP_ORANGE',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_deeporange.min.css']
             },
             'brown': {
                 'id': 'brown',
                 'sample': '#795548',
                 'rgba': 'rgba(121, 85, 72, 0.5)',
                 'name': 'COLOR.BROWN',
-                'theme': ['light']
+                'theme': ['light', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_brown.min.css']
             },
             'grey': {
                 'id': 'grey',
                 'sample': '#9e9e9e',
                 'rgba': 'rgba(33, 150, 243, 1)',
                 'name': 'COLOR.GREY',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_grey.min.css']
             },
             'bluegrey': {
                 'id': 'bluegrey',
                 'sample': '#607d8b',
                 'rgba': 'rgba(96, 125, 139, 0.5)',
                 'name': 'COLOR.BLUE_GREY',
-                'theme': ['light', 'default']
+                'theme': ['light', 'default', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_bluegrey.min.css']
             },
             'darkgrey': {
                 'id': 'darkgrey',
                 'sample': '#2D2D2D',
                 'rgba': 'rgba(45, 45, 45, 0.5)',
                 'name': 'COLOR.DARK_GREY',
-                'theme': ['light']
-            },
-            'endesa': {
-                'id': 'endesa',
-                'sample': '#2196f3',
-                'rgba': 'rgba(33, 150, 243, 0.5)',
-                'name': 'Endesa',
-                'theme': ['light'],
-                'hidden': true
-            },
-
+                'theme': ['light', 'traversed'],
+                'url': ['css/themes/bootstrap-material-design_darkgrey.min.css']
+            }
         };
 
         return {
